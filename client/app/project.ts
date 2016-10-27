601e795eb668f99789c5c99a574676c19fdeb7c9
npqHqJKee <npqHqJKee@gmail.com>
2016-10-27 16:58:00 +0300

    Changes:  left side of main page,  reworked project.ts

--- client/app/project.ts
+++ client/app/project.ts
@@ -1,5 +1,16 @@
 export class Project {
 	id: number;
 	name: string;
-	description: string;
+	region : string;
+	initiators: string[];
+	industry : string;
+	investmentsSummValue : number;
+	investmentsSummCurrency : string;
+	paybackSummValue : number;
+	paybackSummCurrency : string;
+	normalSummValue : number;
+	normalSummCurrency : string;
+	periodSummValue : number;
+	periodSummCurrency : string;
+	description: string[];
 }
